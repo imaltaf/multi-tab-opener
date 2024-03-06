c7eecdadf91eacbe1984c8c66589b33241df2447
imaltaf <imaltaf@proton.me>
2024-03-06 10:46:28 -0500

    added full code into github

---
+++ js/background.js
@@ -0,0 +1,20 @@
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.tabs.query({currentWindow: true}, function (tab) {
+        chrome.tabs.create({ url: "app.html" }, function (tab) { });
+    });
+});
+
+chrome.runtime.onInstalled.addListener(function (tab) {
+    chrome.tabs.create({url: "https://github.com/imaltaf"}, function (tab) { });
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.method == "open") {
+        var link = request.link;
+        var focus = request.focus;
+
+        console.log(link);
+        chrome.tabs.create({ url: link, active: focus });
+    }
+    return true;
+});
---
+++ js/open.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("generateButton").addEventListener("click", generateURL);
+    document.getElementById("copyButton").addEventListener("click", copyToClipboard);
+    document.getElementById("resetButton").addEventListener("click", resetFields);
+});
+
+function generateURL() {
+    const idsInput = document.getElementById('idsInput').value;
+    const urlInput = document.getElementById('urlInput').value;
+    
+    const idsArray = idsInput.split(' ');
+    const generatedURLs = idsArray.map(id => `${urlInput}?id=${id}`).join('\n');
+    
+    document.getElementById('output').value = generatedURLs;
+}
+
+function copyToClipboard() {
+    const output = document.getElementById('output');
+    output.select();
+    document.execCommand('copy');
+    alert('Copied to clipboard!');
+}
+
+function resetFields() {
+    document.getElementById('idsInput').value = '';
+    
+    document.getElementById('output').value = '';
+}
